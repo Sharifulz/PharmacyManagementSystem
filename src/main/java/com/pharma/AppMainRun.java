6a62434d30fc5737d247bdf8afdc74bdbea4612c
Shaariful Islam <20junehackday@gmail.com>
2019-07-02 17:08:23 +0600

    Structured Constructed

---
+++ src/main/java/com/pharma/AppMainRun.java
@@ -0,0 +1,13 @@
+package com.pharma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class AppMainRun {
+	public static void main(String[] args) {
+		SpringApplication.run(AppMainRun.class, args);
+	}
+	
+}
